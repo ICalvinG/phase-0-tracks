0c29824bc7f01505a150fe9872fbe7df18c6d204
Calvin C. Ieong <calvinieong1118@gmail.com>
2016-03-24 02:00:54 -0400

    Wrote and messed around with ruby code for survey

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,12 +1,22 @@
 puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 puts "What year were you born?"
-year = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
-health = gets.chomp
+year = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+garlic = gets.chomp.capitalize
+puts "Would you like to enroll in the company's health insurance? (Y/N)"
+health = gets.chomp.capitalize
 
-puts age
+if age == (2016 - year) && (garlic == "Y" || health == "Y")
+	puts "Probably not a vampire."
+elsif age != (2016 - year) && (garlic == "N" || health == "N")
+	puts "Probably a vampire."
+elsif age != (2016 - year) && (garlic == "N" && health == "N")
+	puts "Almost certainly a vampire."
+elsif name == ("Drake Cula" || "Tu Fang")
+	puts "Definitely a vampire"
+else
+	puts "Results inconclusive"
+end
