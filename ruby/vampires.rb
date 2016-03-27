7db05c65030b7952793f493b282b97337b425438
Calvin C. Ieong <calvinieong1118@gmail.com>
2016-03-27 11:04:41 -0400

    Reformatted if and elsif methods

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,19 +4,20 @@ puts "How old are you?"
 age = gets.chomp.to_i
 puts "What year were you born?"
 year = gets.chomp.to_i
+year = (2016 - year)
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
 garlic = gets.chomp.capitalize
 puts "Would you like to enroll in the company's health insurance? (Y/N)"
 health = gets.chomp.capitalize
 
-if age == (2016 - year) && (garlic == "Y" || health == "Y")
-	puts "Probably not a vampire."
-elsif age != (2016 - year) && (garlic == "N" || health == "N")
-	puts "Probably a vampire."
-elsif age != (2016 - year) && (garlic == "N" && health == "N")
-	puts "Almost certainly a vampire."
-elsif name == ("Drake Cula" || "Tu Fang")
+if name == "Drake Cula" || name == "Tu Fang"
 	puts "Definitely a vampire"
+elsif age == year && garlic == "Y" || health == "Y"
+	puts "Probably not a vampire"
+elsif age != year && garlic == "N" || health == "N"
+	puts "Probably a vampire"
+elsif age != year && (garlic == "Y" && health == "Y")
+	puts "Almost certainly a vampire."
 else
 	puts "Results inconclusive"
 end
