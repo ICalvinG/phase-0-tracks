0e66c1370c78b28e823eab458fe1147f68cf415a
Calvin C. Ieong <calvinieong1118@gmail.com>
2016-03-28 02:27:00 -0400

    Finally got the flow control to work

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -15,27 +15,28 @@ until employees == amount
 	garlic = gets.chomp.capitalize
 	puts "Would you like to enroll in the company's health insurance? (Y/N)"
 	health = gets.chomp.capitalize
-	puts "Name any allergies you may have one at a time. Type in done when finished"
+	puts "Name any allergies you may have one at a time. Type in done when finished."
 	loop do
 		allergies = gets.chomp.capitalize
 		if allergies == "Done"
 			break
 			puts "Thank You!"
 		elsif allergies == "Sunshine"
+			break
 			puts "Probably a vampire"
 		end
 	end
 
 if name == "Drake Cula" || name == "Tu Fang"
-	puts "Definitely a vampire"
+	puts "Definitely a vampire."
 elsif age == year && garlic == "Y" || health == "Y"
-	puts "Probably not a vampire"
-elsif (garlic == "Y" || health == "Y") && age != year
-	puts "Probably a vampire"
+	puts "Probably not a vampire."
 elsif age != year && (garlic == "N" && health == "N") 
 	puts "Almost certainly a vampire."
+elsif (garlic == "Y" && age != year) || && health == "N"
+	puts "Probably a vampire."
 else
-	puts "Results inconclusive"
+	puts "Results inconclusive."
 end
 end 
-puts "Everyone has completed their survey!"
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
