7cb668064cdf2cf1cb327d200359edab75fca69a
Calvin C. Ieong <calvinieong1118@gmail.com>
2016-04-03 00:45:30 -0400

    Wrote code for name input and reversing and joining first and last name.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,11 @@
+#Ask for spy's name
+puts "What is your first name and last name?"
+#Store spy's name into variable
+name = gets.chomp
+#Swap the first and last name
+def swap(name)
+	name.split.reverse.join(' ')
+	p name
+end
+#Change all the vowels to the next vowel 
+#Change all of the consonants into the next consonant
