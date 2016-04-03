86e014eb852635f11b8a0cfdc5bd606e5f4f4a15
Calvin C. Ieong <calvinieong1118@gmail.com>
2016-04-03 02:53:57 -0400

    Fixing hash release

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,12 +1,23 @@
-#Ask for spy's name
-puts "What is your first name and last name?"
-#Store spy's name into variable
-name = gets.chomp
 #Swap the first and last name
 #Change all the vowels to the next vowel
 #Change all of the consonants into the next consonant
 def secret(name)
 	name.split.reverse.join(' ').tr('abcdefghijklmnopqrstuvwxyz', 'ecdfighjoklnpuqrstvawxyzb')
 end
+#Create hash for storage of fake names
+secret_identities = {}
+#Ask for spy's name
+puts "What is your first name and last name? Type Quit to end."
+#Store spy's name into variable and create loop
+loop do
+	name = gets.chomp
+	if name.capitalize == "Quit"
+		puts "Good Luck!"
+		break
+	else
+		p secret(name)
+	end
+end
 
-p secret(name)
+secret_identities[name] = secret(name)
+p secret_identities
