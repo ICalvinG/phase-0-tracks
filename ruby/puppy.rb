4b3b67a6e7d6e847b8ad7ff43beae8226eaa87cf
Calvin C. Ieong <calvinieong1118@gmail.com>
2016-04-04 18:05:20 -0400

    Added in characteristics and behavior for puppy class.

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+#Puppy
+
+#Name - Hunter
+
+#Species
+#Husky
+
+#Characteristics
+#Fur Color - White/Brown
+#Eye Color - Blue
+#Size - Medium
+#Legs - Long
+#Fur - Long Hair
+
+#Behavior
+#Very Smart
+#Loves to run around
+#Loves to be given attention
+#Drools a lot
+#Sleeps a lot
