e79a5d2af4dabbade104d8c808453e33284167a8
Calvin C. Ieong <calvinieong1118@gmail.com>
2016-04-08 00:21:58 -0400

    Fixed the code for everything, still have issue with reindeer rankings.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
+	attr_accessor :gender, :ethnicity
+	attr_reader :age
 	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
+	    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = rand(140)
 		puts "Initializing Santa instance..."
 	end
 	def speak
@@ -10,29 +14,32 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
-	def reindeer_ranking
-		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	end
-	def age
-		@age = 0
-		celebrate_birthday
-	end
 	def celebrate_birthday
-		if @age == 0
-			@age = @age +1
-		end
+		@age += 1
 	end
 	def get_mad_at(reindeer)
-		reindeer.
+		bad_reindeer = @reindeer_ranking.delete(reindeer)
+		@reindeer_ranking.push(bad_reindeer)
+	end
+	def gender
+		@gender
+	end
+	def ethnicity
+		@ethnicity
+	end
 end
 
 claus = Santa.new("male", "asian")
 claus.speak
 claus.eat_milk_and_cookies("chocolate chip")
+claus.celebrate_birthday
+claus.get_mad_at("Vixen")
+puts "Santa is a #{claus.gender} and he is also #{claus.ethnicity}!"
 
 santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "monster"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Singaporean"]
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "monster", "mutant", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "lizard", "bird", "plane"]
 example_genders.length.times do |i|
   santas << Santa.new(example_genders[i], example_ethnicities[i])
+  p santas
 end
