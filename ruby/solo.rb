baf28b8f9f3a8c93dd923396e733bccfcd511cf5
Calvin C. Ieong <calvinieong1118@gmail.com>
2016-04-11 02:01:52 -0400

    Added in code for correct name, added in accessor and reader tags, and also checked to see if code works.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,7 +1,7 @@
 #Create a class for Turtle.
 class Turtle
 	attr_reader :color
-	attr_accessor :name :age 
+	attr_accessor :name, :age 
 #Give Turtle three attributes.
 	def initialize(name, age)
 #Attributes will be name, age, and color.
@@ -12,14 +12,19 @@ class Turtle
 #Give Turtle three methods.
 #One method will be hide method where turtle hides in shell.
 	def hide
-		puts "*hides* Turtle is now hiding in his shell."
+		puts "*hides* #{@name} is now hiding in his shell."
 	end
 #One method will be chew method where turtle chews food.
 	def chew(food)
-		puts "*chew* Turtle is now chewing a #{food}"
+		puts "*chew* #{@name} is now chewing a #{food}"
 	end
 #One method will be walk method where turtle takes one step.
-	def walk(step = 0)
-		step += 1
+	def walk
+		puts "*step* #{@name} just took a step. Hooray!"
 	end
 end
+
+shell = Turtle.new("Michelangelo", 2)
+shell.hide
+shell.chew("cookie")
+shell.walk
