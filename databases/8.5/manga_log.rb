86a2dcf350efada4cc6326f2d37378cc67f68749
Calvin C. Ieong <calvinieong1118@gmail.com>
2016-04-24 03:08:26 -0400

    Wrote in code to create a database and added in code to create a table within database.

--- databases/8.5/manga_log.rb
+++ databases/8.5/manga_log.rb
@@ -1,5 +1,30 @@
 # Manga Log Application
 
-# require gems
+# require gem
 require 'sqlite3'
-require 'faker'
+
+# Create the SQLite3 database
+manga_db = SQLite3::Database.new("manga_read.db")
+manga_db.results_as_hash = true
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS manga(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    volumes INT,
+    read BOOLEAN,
+    rating INT
+  )
+SQL
+
+# Create a table for all the read manga
+manga_db.execute(create_table_cmd)
+
+# Create a method to add a manga
+def add_manga(db, name, volumes, read, rating)
+	db.execute("INSERT INTO manga (name, volumes, read, rating) VALUES (?, ?, ?, ?)", [name, volumes, read, rating])
+end
+# Create a method to update volumes
+def update_volumes(db, name, volumes)
+	db.execute("UPDATE manga SET volumes=? WHERE name=?", [volumes, name])
+end
